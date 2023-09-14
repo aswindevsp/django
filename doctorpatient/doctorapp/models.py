c188638396a9e91f21d388ac580c1eb20fb6cbba
aswindevsp <aswindevsp@gmail.com>
2023-09-14 11:57:02 +0530

    ini

--- doctorpatient/doctorapp/models.py
+++ doctorpatient/doctorapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    username = models.CharField(max_length=100,default="doctor")
+    password = models.CharField(max_length=100, default="doctor")
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    mobile = models.CharField(max_length=10)
+    speciality = models.CharField(max_length=100)
+    experience = models.IntegerField()
+    qualification = models.CharField(max_length=100)
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    mobile = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
--- doctorpatient/doctorapp/views.py
+++ doctorpatient/doctorapp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
+from .models import Doctor, Patient
+
 # Create your views here.
 def homePageView(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        if username == 'doctor' and password == 'doctor':
-            return render(request, 'doctor.html')
-        elif username == 'admin' and password == 'admin':
+        
+        if username == 'admin' and password == 'admin':
             return render(request, 'admin.html')
+        elif Doctor.objects.filter(username=username, password=password).exists():
+            return render(request, 'doctor.html')
         else:
             return render(request, 'login.html')
     else:
@@ -22,15 +25,50 @@ def adminPageView(request):
     return render(request, 'admin.html')
 
 def addPatientPageView(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        mobile = request.POST['mobile']
+        address = request.POST['address']
+        patient = Patient(
+            name=name, 
+            email=email, 
+            mobile=mobile, 
+            address=address) 
+        
+        patient.save()
     return render(request, 'addPatient.html')
 
 def addDoctorPageView(request):
+    print("working? ")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        name = request.POST['name']
+        email = request.POST['email']
+        mobile = request.POST['mobile']
+        speciality = request.POST['speciality']
+        experience = request.POST['experience']
+        qualification = request.POST['qualification']
+        doctor = Doctor(
+            username=username, 
+            password=password, 
+            name=name, 
+            email=email, 
+            mobile=mobile, 
+            speciality=speciality, 
+            experience=experience, 
+            qualification=qualification)
+        
+        doctor.save()
     return render(request, 'addDoctor.html')
 
 def listPatientPageView(request):
-    return render(request, 'listPatient.html')
+    patients = Patient.objects.all()
+    return render(request, 'listPatient.html', {'patients': patients})
 
 def listDoctorPageView(request):
-    return render(request, 'listDoctor.html')
+    docotors = Doctor.objects.all()
+    return render(request, 'listDoctor.html', {'doctors': docotors})
 
 
